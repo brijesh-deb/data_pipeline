2effb8029ab6ae1af9a69ad12530ce7a259e0315
Brijesh Deb <brijesh_deb@yahoo.com>
2020-12-30 12:10:26 +0530

    Add files via upload

---
+++ Airflow/spark_dag.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+dag = DAG('spark_job_dag', description='DAG to trigger pySpark job',
+          schedule_interval='0 12 * * *',
+          start_date=datetime(2020, 3, 20), catchup=False)
+
+start_task = DummyOperator(task_id='start_task', dag=dag)
+
+commands = """
+    cd /Users/philips/Documents/Study_code/Spark/spark-samples;
+    /usr/local/Cellar/apache-spark/3.0.1/bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 fetch_data.py;
+    """
+
+spark_task = BashOperator(
+    task_id='spark-task',
+    bash_command=commands,
+    dag=dag)
+
+start_task >> spark_task
